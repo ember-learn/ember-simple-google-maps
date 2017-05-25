54a191ecb3d5d99f79bab02e51cf9bf11c0f37e7
Todd Jordan <todd.jordan@inin.com>
2017-05-24 22:22:16 -0400

    Inject the Google Maps script into the parent application

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-simple-google-maps'
+  name: 'ember-simple-google-maps',
+
+  contentFor(type, config) {
+    let scriptUrl = '';
+    if (type === 'body-footer') {
+      scriptUrl = `${config[this.name].url}?v=${config[this.name].version}`
+      if (config[this.name].apiKey !== '') {
+        scriptUrl = scriptUrl + `&key=${config[this.name].apiKey}`;
+      }
+    }
+    return scriptUrl !== '' ? `<script src="${scriptUrl}"></script>` : '';
+  }
+
 };
